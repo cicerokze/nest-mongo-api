b76e3147b6725ebb0d480b57fecb36e5ee0ff34a
kzekhaos <cicerocaze@gmail.com>
2024-06-26 22:52:22 -0300

    feat(dto): adiciona modelo dto para usuário

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,11 @@
+export class UserDto {
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly email: string;
+    readonly password: string;
+    readonly cep: string;
+    readonly street: string;
+    readonly number: string;
+    readonly city: string;
+    readonly state: string;
+}
