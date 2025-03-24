53dc7ea8db520486b31d6369a6ef93271477fa92
Cicero Caze <cicerocaze@gmail.com>
2025-03-24 07:46:46 +0000

    feat(settings): add db settings to database module

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,13 @@
 import * as mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+const db = process.env.DB_URI || 'mongodb://localhost/nest-mongo-api';
 
 export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: (): Promise<typeof mongoose> =>
-            mongoose.connect('mongodb://localhost/nest-mongo-api'),
+            mongoose.connect(db),
     },
 ];
